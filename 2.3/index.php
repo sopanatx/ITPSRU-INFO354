238f23098c8de3b31576099a4108d86189f2332a
Sopanat Suradach <sopanus2014@gmail.com>
2021-01-25 12:33:43 +0700

    Initial Ex3.0

--- 2.3/index.php
+++ 2.3/index.php
@@ -15,6 +15,7 @@ th,td,button {
 }
 </style>
 <head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>แสดงผล XML </title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 <link rel="preconnect" href="https://fonts.gstatic.com">
